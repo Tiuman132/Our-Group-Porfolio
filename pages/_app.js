bdbf7764f1d2ca7c6d1e942eb2f57f0cb548976d
Tiuman132 <tieuman132@gmail.com>
2025-08-15 23:49:51 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 "use client";
 
-import '../styles/globals.css';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import 'styles/globals.css';
+import Navbar from 'components/Navbar';
+import Footer from 'components/Footer';
 
 export default function App({ Component, pageProps }) {
   if (typeof Navbar !== 'function' || typeof Footer !== 'function') {
