64d741b9363566f3b9d3dc2f228ab19f14229522
Tiuman132 <tieuman132@gmail.com>
2025-08-16 00:35:40 -0400

    Update Footer.js

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,6 +1,6 @@
 "use client";
 
-import styles from '../styles/Footer.module.css';
+import styles from '@/styles/Footer.module.css';
 
 export default function Footer() {
     return (
@@ -15,4 +15,5 @@ export default function Footer() {
     );
 
 }
+
 
