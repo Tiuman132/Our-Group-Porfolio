da621be4e6e0232e2a00f3dfd725f604d301bf9c
Tiuman132 <tieuman132@gmail.com>
2025-08-16 00:34:42 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import Head from 'next/head';
-import styles from './styles/Home.module.css';
+import styles from '@/styles/Home.module.css';
 
 export default function Home() {
   const members = [
